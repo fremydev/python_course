50d7f3da6a838720ad620810f40efadf0d06d8f3
fremydev <fremy.dev@gmail.com>
2023-07-20 23:08:08 -0300

    Variables

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello world")
+first_name: str = "Bro"
+last_name: str = "lolo"
+full_name: str = first_name + " " + last_name
+#print(type(name))
+print("Hello " + full_name)
+
+age: int = 23
+age += 1
+#print(type(age))
+print(age)
+print("Your age is: " + str(age))
+
+height: float = 170.5
+#print(type(height))
+print(height)
+print("Your height is: " + str(height) + "cm")
+
+human: bool = False
+#print(type(human))
+print("Are you a human: " + str(human))
+print(human)
+
+name, age, attractive = "Bro", 23, True
+
+print(name)
+print(age)
+print(attractive)
+
+spongebob = patrick = sandy = squidward = 30
+
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
